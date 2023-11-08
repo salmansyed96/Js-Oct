a0ebed6dd514b48438ddcdc4da468372d177c6d3
salmansyed96 <124539970+salmansyed96@users.noreply.github.com>
2023-11-08 11:52:07 +0530

    Datatype conversion confusion

---
+++ 01DataType.js
@@ -0,0 +1,6 @@
+//"Use Strict"; // treat all js code as newer version
+
+let someNUmber = 33;
+let stringNumber = String(someNUmber);
+console.log(typeof stringNumber);
+console.log(someNUmber);
---
+++ Const.js
@@ -0,0 +1,6 @@
+const Acount = 12345;
+let accoutID = "Syed@gmail";
+var password = 12121212;
+acountCity = "BHopal"
+
+console.table([Acount, accoutID, password, acountCity])
